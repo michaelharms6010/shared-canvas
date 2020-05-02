221599200f86fb3e88d9a7143fd2021a5ad7c902
Michael Harms <michael.harms6010@gmail.com>
2020-05-01 21:55:45 -0500

    removes boring package

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
-import CanvasDraw from "react-canvas-draw";
 
 function App() {
+
+
   return (
     <div className="App">
-      <CanvasDraw 
-        canvasWidth="1000px"
-        canvasHeight ="1000px"
-       
-      />
+      
     </div>
   );
 }
